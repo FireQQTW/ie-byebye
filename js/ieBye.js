01ed407cad07562bc712391b3bb89d928ead245e
Casper Lai <casper.lai@sleepingdesign.com>
2014-01-08 12:02:11 +0800

    Redefined the variable name.  replace attr to prop method.

--- js/ieBye.js
+++ js/ieBye.js
@@ -5,22 +5,23 @@
  * @version 0.1
  */
 $(document).ready(function() {
-	var 	_firefixURL = 'http://moztw.org/firefox/download/latest-win.html',
-		_chromeURL ='https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B9992E29F-C177-515E-4DD0-1A87BDB6ADD3%7D%26lang%3Dzh-TW%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26brand%3DCHMB%26installdataindex%3Ddefaultbrowser/update2/installers/ChromeSetup.exe',
-		_ieURL = 'http://windows.microsoft.com/zh-tw/internet-explorer/ie-10-worldwide-languages',
-		_pText = '提醒您，IE瀏覽器版本過舊，為了提升您的使用者體驗，建議您更換瀏覽器。',
-		_barWrap = '<div class="ieByeBar"><p>#</p><div class="BtnWrap"><a href="#" target="_blank" class="goFiefox">FIREFOX</a><a href="#" target="_blank" class="goChrome">CHROME</a><a href="#" target="_blank" class="goIE">IE</a></div><a href="#" class="closeBtn">CLOSE</a></div>';
+	var browserURL = {
+		firefox: 'http://moztw.org/firefox/download/latest-win.html',
+		chrome: 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B9992E29F-C177-515E-4DD0-1A87BDB6ADD3%7D%26lang%3Dzh-TW%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26brand%3DCHMB%26installdataindex%3Ddefaultbrowser/update2/installers/ChromeSetup.exe',
+		IE: 'http://windows.microsoft.com/zh-tw/internet-explorer/ie-10-worldwide-languages'
+	};
+	var tip = '提醒您，IE瀏覽器版本過舊，為了提升您的使用者體驗，建議您更換瀏覽器。';
+	var template = '<div class="ieByeBar"><p>#</p><div class="BtnWrap"><a href="#" target="_blank" class="goFiefox">FIREFOX</a><a href="#" target="_blank" class="goChrome">CHROME</a><a href="#" target="_blank" class="goIE">IE</a></div><a href="#" class="closeBtn">CLOSE</a></div>';
 	
 	//將基本的結構寫進網頁
-	$('body .ieByeBye').before(_barWrap);
-
+	$('body .ieByeBye').before(template);
 	//寫入說明文字
-	$('.ieByeBar p').text(_pText);
-
+	$('.ieByeBar p').text(tip);
+	
 	//將三個瀏覽器的href寫入
-	$('.goFiefox').attr('href',	_firefixURL);
-	$('.goChrome').attr('href', _chromeURL);
-	$('.goIE').attr('href', _ieURL);
+	$('.goFiefox').prop('href',	browserURL.firefox);
+	$('.goChrome').prop('href', browserURL.chrome);
+	$('.goIE').prop('href', browserURL.IE);
 
 	//關閉按鈕
 	$('.ieByeBar .closeBtn').click(function(event) {
