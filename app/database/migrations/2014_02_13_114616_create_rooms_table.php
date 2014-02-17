a94039a7934569522a7b4eb768d91bb4d81f13e2
Casper Lai <casper.lai@sleepingdesign.com>
2014-02-17 14:52:42 +0800

    Fix room password length too short.

--- app/database/migrations/2014_02_13_114616_create_rooms_table.php
+++ app/database/migrations/2014_02_13_114616_create_rooms_table.php
@@ -18,7 +18,7 @@ class CreateRoomsTable extends Migration {
 			$table->string('sn', 32)->uniqid();
 			$table->integer('house_id')->unsigned()->index();
 			$table->string('username', 50)->nullable()->index();
-			$table->string('password', 32)->nullable()->index();
+			$table->string('password', 60)->nullable()->index();
 			$table->string('name', 255);
 			$table->decimal('billed', 10, 0);
 			$table->string('BilledTypeJsonData', 255)->nullable();
