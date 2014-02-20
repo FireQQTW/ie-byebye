b1ec7b36eeef83619f8e14df197d3a5077a747bd
Casper Lai <casper.lai@sleepingdesign.com>
2014-02-20 21:27:59 +0800

    Fix model fail.

--- app/views/admin/rooms/create.blade.php
+++ app/views/admin/rooms/create.blade.php
@@ -4,7 +4,7 @@
 	{{HTML::script('/admin/js/houses/form.js')}}
 @stop
 @section('main')
-	{{ Form::model(new \House, array('route' => array('admin.houses.rooms.store', $house->sn), 'class' => 'form-horizontal', 'role' => 'form')) }}
+	{{ Form::model(new \Room, array('route' => array('admin.houses.rooms.store', $house->sn), 'class' => 'form-horizontal', 'role' => 'form')) }}
         @include('admin/rooms/_partials/_form', ['submit_text' => '儲存'])
 	{{ Form::close() }}
 @stop
