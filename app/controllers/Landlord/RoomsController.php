088af7da78c19299492ce1dd1f22f11d88477311
Casper Lai <casper.lai@sleepingdesign.com>
2014-02-25 11:47:39 +0800

    Add bill resource.

--- app/controllers/Landlord/RoomsController.php
+++ app/controllers/Landlord/RoomsController.php
@@ -103,11 +103,12 @@ class RoomsController extends \BaseController {
 	 */
 	public function update($house_sn, $room_sn)
 	{
-		$h1Small = '變更密碼';
 		try
 		{
 			$room = \Room::where('sn', '=', $room_sn)->firstOrFail();
 			$room->checkOwner($this->Landlord->id);
+			// if password empty, would not confirm or update password.
+			$room::$rules['password'] = (\Input::get('password')) ? $room::$rules['password'] : '';
 			if($room->save())
 			{
 				return Redirect::route('landlord.rooms', array($house_sn))->with('message', "修改完成");
@@ -115,6 +116,83 @@ class RoomsController extends \BaseController {
 			else
 			{
 				return Redirect::route('landlord.rooms.edit', array($house_sn, $room_sn))
+					->withInput()
+					->withErrors($room->errors()); 
+			}
+		}
+		catch (ModelNotFoundException $e)
+		{
+			return Redirect::route('landlord.rooms', array($house_sn))->with('message', '無此筆資料，請檢查。');
+		}
+		catch (NotOwnerException $e)
+		{
+			return Redirect::route('landlord.rooms', array($house_sn))->with('message', '資料授權失敗。');
+		}
+	}
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @param  string  $house_sn
+	 * @param  string  $room_sn
+	 * @return Response
+	 */
+	public function bill($house_sn, $room_sn)
+	{
+		$h1Small = '計價設定';
+		try
+		{
+			$room = \Room::where('sn', '=', $room_sn)->firstOrFail();
+			$room->checkOwner($this->Landlord->id);
+			return View::make('landlord.rooms.bill', compact('h1Small', 'room'));
+		}
+		catch (ModelNotFoundException $e)
+		{
+			return Redirect::route('landlord.rooms', array($house_sn))->with('message', '無此筆資料，請檢查。');
+		}
+		catch (NotOwnerException $e)
+		{
+			return Redirect::route('landlord.rooms', array($house_sn))->with('message', '資料授權失敗。');
+		}
+	}
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @param  string  $house_sn
+	 * @param  string  $room_sn
+	 * @return Response
+	 */
+	public function bill_update($house_sn, $room_sn)
+	{
+		try
+		{
+			$room = \Room::where('sn', '=', $room_sn)->firstOrFail();
+			$room->checkOwner($this->Landlord->id);
+			// if password empty, would not confirm or update password.
+			$room::$rules['password'] = (\Input::get('password')) ? $room::$rules['password'] : '';
+			$billValue = \Input::get('billValue');
+			$billType = \Input::get('billType');
+			// manual validation.
+			$manualValidationErrors = array();
+			if(empty($billType))
+				$manualValidationErrors[] = '請選擇計價方式';
+			if($billValue <= 0 || empty($billValue))
+				$manualValidationErrors[] = '請輸入費用';
+			if(count($manualValidationErrors) > 0)
+				return Redirect::route('landlord.rooms.edit.bill', array($house_sn, $room_sn))
+					->withInput()
+					->with('message', $manualValidationErrors)
+					->with('style', 'error');
+
+			// combine bill value and type to json
+	        $billJson = json_encode(array("value" =>  $billValue,   "radio" =>  $billType));
+	        $room->BilledTypeJsonData = $billJson;
+			if($room->save())
+			{
+				return Redirect::route('landlord.rooms', array($house_sn))->with('message', "修改完成");
+			}
+			else
+			{
+				return Redirect::route('landlord.rooms.edit.bill', array($house_sn, $room_sn))
 					->withInput()
 					->withErrors($room->errors()); 
 			}
---
+++ app/views/landlord/rooms/bill.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.landlord')
+
+@section('breadcrumbs', Breadcrumbs::render('landlord.rooms.edit.bill', $room))
+@section('main')
+    {{ Form::model($room, array('method'   =>  'POST', 'route' => array('landlord.rooms.update.bill', $room->house->sn, $room->sn), 'class' => 'form-horizontal', 'role' => 'form')) }}
+         <div class="form-group">
+             {{ Form::label('billType', '計價方式', array('class' => 'col-sm-3 control-label no-padding-right')) }}
+            <div class="radio col-sm-9">
+                <label class="inline">
+                    {{ Form::radio('billType', 'walt', $room->getBilledType() == 'walt' ? true : false, array('class'    =>  'ace')) }}
+                    <span class="lbl">
+                        每度
+                    </span>
+                </label>
+                <label class="inline">
+                    {{ Form::radio('billType', 'time', $room->getBilledType() == 'time' ? true : false, array('class'    =>  'ace')) }}
+                    <span class="lbl">
+                        每小時
+                    </span>
+                </label>
+            </div>
+        </div>
+
+        <div class="space-4"></div>
+        <div class="form-group">
+            {{ Form::label('billValue', '費用', array('class' => 'col-sm-3 control-label no-padding-right')) }}
+            <div class="col-sm-9">
+                {{ Form::text('billValue', $room->getBilledUnitPrice() ,array('class'   =>  'col-xs-10 col-sm-5', 'placeholder' =>  '請輸入費用')) }}
+            </div>
+        </div>
+
+        <div class="space-4"></div>
+        @include('_partials/_form_error', ['errors'  =>  $errors])
+        <div class="clearfix form-actions">
+            <div class="col-md-offset-3 col-md-9">
+                {{ HTML::ButtonWithIcon('<i class="icon-ok bigger-110"></i>儲存', array('class' => 'btn btn-info', 'type' => 'submit')) }}
+
+                &nbsp; &nbsp; &nbsp;
+
+                {{ HTML::decode(link_to_route('landlord.rooms', '<i class="icon-remove bigger-110"></i>取消', array($room->house->sn), array('class' => 'btn'))) }}
+            </div>
+        </div>
+    {{ Form::close() }}
+@stop
