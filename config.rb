d89690426e2f05f73a8e17774bf5389ab434a6ca
Casper Lai <casper.lai@sleepingdesign.com>
2014-02-26 15:11:56 +0800

    Add fireapp config.

---
+++ config.rb
@@ -0,0 +1,6 @@
+javascripts_dir = "public/js" # by Fire.app 
+fireapp_coffeescripts_dir = "src/coffeescripts" # by Fire.app 
+fireapp_minifyjs_on_build = false # by Fire.app 
+fireapp_always_report_on_build = true # by Fire.app 
+fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
+Encoding.default_external = "UTF-8"
